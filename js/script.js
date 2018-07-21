c1c71ac53e94d2eac0279f7ae572af970b697486
drewatienza <atienza.drew@gmail.com>
2018-07-21 12:25:50 -0700

    added typed.js for the sub heading

--- js/script.js
+++ js/script.js
@@ -6,4 +6,12 @@ $(document).ready(function() {
     pagination: false
   });
 
+  var typed = new Typed(".typed", {
+    strings: ["Software Engineer.", "Web Developer.", "Student."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
+
 });
